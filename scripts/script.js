eaa7ef44c4c436b3d1ac9729f39a73876ce66a52
Alan Chen <alanchen1908@gmail.com>
2019-09-17 17:19:08 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 var currViewportHeight;
+var minHeight
 
 $(document).ready( function() {
 	$("#arrow-down").click(function() {
@@ -10,54 +11,70 @@ $(document).ready( function() {
   	$('[data-toggle="tooltip"]').tooltip();
 	});
 	var viewport = $(this); 
-    var viewportHeight = viewport.height();
-    var viewportWidth = viewport.width();
-    document.getElementById("content-wrapper").style.height = 100 + "%";
-    minHeight = $("#content-wrapper").height();
-    console.log(minHeight);
-    document.getElementById("splash-wrapper").style.height = viewportHeight + "px";
-    if(viewportHeight > minHeight+144) {
-  		document.getElementById("content-wrapper").style.height = viewportHeight + "px";
-    } else {
-  		document.getElementById("content-wrapper").style.height = 100 + "%";
-    }
-    document.getElementById("splash-inner-wrapper").style.width = viewportWidth + "px";
-    currViewportHeight = viewportHeight;
-    $("#arrow-down-wrapper").removeClass("animated");
-    $("#arrow-down-wrapper").removeClass("animatedFadeInUp");
-    $("#arrow-down-wrapper").removeClass("fadeInUp");
+  var viewportHeight = viewport.height();
+  var viewportWidth = viewport.width();
+  document.getElementById("content-wrapper").style.height = 100 + "%";
+  minHeight = $("#content-wrapper").height();
+  console.log(minHeight);
+  document.getElementById("splash-wrapper").style.height = viewportHeight + "px";
+  if(viewportHeight > minHeight+144) {
+  	document.getElementById("content-wrapper").style.height = viewportHeight + "px";
+  } else {
+  	document.getElementById("content-wrapper").style.height = 100 + "%";
+  }
+  document.getElementById("splash-inner-wrapper").style.width = viewportWidth + "px";
+  currViewportHeight = viewportHeight;
+  
+  var imageWidth = viewportWidth * 0.2;
+  if(imageWidth < 300) {
+    document.getElementById("self-image").style.width = imageWidth + "px";
+    document.getElementById("self-image").style.height = imageWidth + "px";
+  } else {
+    document.getElementById("self-image").style.width = "300px";
+    document.getElementById("self-image").style.height = "300px";
+  }
+  
+  $("#arrow-down-wrapper").removeClass("animated");
+  $("#arrow-down-wrapper").removeClass("animatedFadeInUp");
+  $("#arrow-down-wrapper").removeClass("fadeInUp");
 });
 
 $(window).on('resize', function(){
-    var viewport = $(this); 
-    var viewportHeight = viewport.height();
-    var viewportWidth = viewport.width();
-	document.getElementById("content-wrapper").style.height = 100 + "%";
-    minHeight = $("#content-wrapper").height();
-    document.getElementById("splash-wrapper").style.height = viewportHeight + "px";
-    if(viewportHeight > minHeight+144) {
-  		document.getElementById("content-wrapper").style.height = viewportHeight + "px";
-  	} else {
-  		document.getElementById("content-wrapper").style.height = 100 + "%";
-  	}
-    document.getElementById("splash-inner-wrapper").style.width = viewportWidth + "px";
-    currViewportHeight = viewportHeight;
+      var viewport = $(this); 
+      var viewportHeight = viewport.height();
+      var viewportWidth = viewport.width();
+      document.getElementById("content-wrapper").style.height = 100 + "%";
+  		minHeight = $("#content-wrapper").height();
+      document.getElementById("splash-wrapper").style.height = viewportHeight + "px";
+      if(viewportHeight > minHeight+144) {
+  			document.getElementById("content-wrapper").style.height = viewportHeight + "px";
+  		} else {
+  			document.getElementById("content-wrapper").style.height = 100 + "%";
+  		}
+      document.getElementById("splash-inner-wrapper").style.width = viewportWidth + "px";
+      currViewportHeight = viewportHeight;
+      
+      var imageWidth = viewportWidth * 0.2;
+      if(imageWidth < 300) {
+      	document.getElementById("self-image").style.width = imageWidth + "px";
+      	document.getElementById("self-image").style.height = imageWidth + "px";
+      } else {
+      	document.getElementById("self-image").style.width = "300px";
+        document.getElementById("self-image").style.height = "300px";
+      }
 });
 
 $(window).scroll(function(){
     $(".splashFade").removeClass("animated");
     $(".splashFade").removeClass("animatedFadeInUp");
     $(".splashFade").removeClass("fadeInUp");
-    $(".splashFade").css("opacity", 1 - $(window).scrollTop() / (currViewportHeight/2));
+    $(".splashFade").css("opacity", 0.999 - $(window).scrollTop() / (currViewportHeight/2));
 });
 
-//Secret :)
 $("#splashTwitter").click(function() {
     window.location = "https:/twitter.com/nodinawe";
 });
-/*
+
 $("#splashLinkedIn").click(function() {
-    window.location = "https:/linkedin.com/in/alan-chen-a07034171";
-    console.log("clicked");
+    window.location = "linkedin.com/in/alan-chen-a07034171";
 });
-*/
